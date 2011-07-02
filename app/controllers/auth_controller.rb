44ceb5f1da00fe7c73a3071c0d8f319fccf9a0e2
Jeff Dickey <dickeytk@gmail.com>
2011-07-02 16:46:18 -0700

    Implemented callback

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,16 +2,21 @@ require 'oauth2'
 
 class AuthController < ApplicationController
   def login
-    client = OAuth2::Client.new(ENV['MEETUP_APP_KEY'], ENV['MEETUP_APP_SECRET'], :authorize_url => 'https://secure.meetup.com/oauth2/authorize')
     redirect_to client.web_server.authorize_url(
       :redirect_uri => redirect_uri
     )
   end
 
   def callback
+    access_token = client.web_server.get_access_token(params[:code], redirect_uri => redirect_uri)
+    render :text => access_token
   end
 
   private
+
+  def client
+    OAuth2::Client.new(ENV['MEETUP_APP_KEY'], ENV['MEETUP_APP_SECRET'], :authorize_url => 'https://secure.meetup.com/oauth2/authorize')
+  end
 
   def redirect_uri
     uri = URI.parse(request.url)
