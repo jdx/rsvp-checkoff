6dd3b2cc9cd43a54561af712ac534e797254efe9
Luca Candela <luca.candela@gmail.com>
2011-07-02 18:48:47 -0700

    added toolbar

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -1,20 +1,26 @@
 var App = new Ext.Application({
-	name: 'rsvp',
+	name: 'rsvpApp',
 	useLoadMask: true,
 	launch: function () {
 	
-	rsvp.views.meetupListContainer = new Ext.Panel({
-		id: 'meetupList',
-		layout: 'fit',
-		html: 'this is the meetup list container'
+	rsvpApp.views.meetupToolbar = new Ext.Toolbar ({
+		id: 'meetupToolbar',
+		title: 'RSVP List'
 	});
 	
 	
+	rsvpApp.views.meetupListContainer = new Ext.Panel({
+		id: 'meetupList',
+		layout: 'fit',
+		html: 'this is the meetup list container'
+		dockedItems: [rsvpApp.views.meetupToolbar]
+	});
 	
-	rsvp.views.viewport = new Ext.Panel({
+	
+	rsvpApp.views.viewport = new Ext.Panel({
 		fullscreen: true,
 		layout: 'card',
 		cardAnimation: 'slide',
-		items: [rsvp.views.meetupListContainer]
+		items: [rsvpApp.views.meetupListContainer]
 	});
 }});
