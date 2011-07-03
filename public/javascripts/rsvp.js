cbf99d5c1907f3d66c61a98f4d1c439b16a5374c
Luca Candela <luca.candela@gmail.com>
2011-07-02 18:41:48 -0700

    small fix

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -2,8 +2,19 @@ var App = new Ext.Application({
 	name: 'rsvp',
 	useLoadMask: true,
 	launch: function () {
+	
+	rsvp.views.meetupListContainer = new Ext.Panel({
+		id: 'meetupList',
+		layout: 'fit',
+		html: 'this is the meetup list container'
+	});
+	
+	
+	
 	rsvp.views.viewport = new Ext.Panel({
 		fullscreen: true,
-		html:'this is a viewport'
+		layout: 'card',
+		cardAnimation: 'slide',
+		items: [rsvp.views.meetupListContainer]
 	});
 }});
