0b9561ed34ba167d50bbe63edea95860b2fbe934
Luca Candela <luca.candela@gmail.com>
2011-07-02 23:59:32 -0700

    added data store for user list

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -15,24 +15,48 @@ var App = new Ext.Application({
       window.location = 'https://secure.meetup.com/oauth2/authorize?client_id=' + client_id + '&response_type=token&redirect_uri=' + encodeURIComponent(document.URL);
     }
 
-    rsvpApp.views.meetupToolbar = new Ext.Toolbar ({
-      id: 'meetupToolbar',
+    rsvpApp.views.mainToolbar = new Ext.Toolbar ({
+      id: 'mainToolbar',
       title: 'RSVP List'
     });
 
-    rsvpApp.views.meetupListContainer = new Ext.Panel({
-      id: 'meetupList',
+    rsvpApp.views.mainContainer = new Ext.Panel({
+      id: 'mainList',
       layout: 'fit',
       html: 'this is the meetup list container',
-      dockedItems: [rsvpApp.views.meetupToolbar]
+      dockedItems: [rsvpApp.views.mainToolbar]
     });
 
     rsvpApp.views.viewport = new Ext.Panel({
       fullscreen: true,
       layout: 'card',
       cardAnimation: 'slide',
-      items: [rsvpApp.views.meetupListContainer]
+      items: [rsvpApp.views.mainContainer]
     });
   }
-
 });
+
+eventStore = Ext.extend(Ext.data.JsonStore, {
+    constructor: function(cfg) {
+        cfg = cfg || {};
+        eventStore.superclass.constructor.call(this, Ext.apply({
+            storeId: 'Event',
+            url: 'https://api.meetup.com/2/rsvps?key=387c221de2f734b2e31591d1550637d&sign=true&event_id=16336453',
+            batch: true,
+            autoLoad: true,
+            root: 'results',
+            autoSave: false,
+            fields: [
+                { name: 'response', type: 'string'},
+                { name: 'name', type: 'string', mapping: '[\'member\'][\'name\']'},
+                { name: 'member_id', type: 'string', mapping: '[\'member\'][\'member_id\']'},
+                { name: 'photo thumbs', type: 'string', mapping: '[\'member_photo\'][\'thumb_link\']'}
+                { name: 'answers', type: 'string'},
+                { name: 'guests', type: 'int'},
+                { name: 'commments', type: 'string'}
+            ]
+        }, cfg));
+    }
+});
+
+new eventStore();
