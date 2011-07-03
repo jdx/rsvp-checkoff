aa38654a3a03e69d6a24bfbb95d2904004cd9bce
Jeff Dickey <dickeytk@gmail.com>
2011-07-02 23:46:51 -0700

    Added inherit authentication

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -1,26 +1,39 @@
 var App = new Ext.Application({
-	name: 'rsvpApp',
-	useLoadMask: true,
-	launch: function () {
-	
-	rsvpApp.views.meetupToolbar = new Ext.Toolbar ({
-		id: 'meetupToolbar',
-		title: 'RSVP List'
-	});
-	
-	
-	rsvpApp.views.meetupListContainer = new Ext.Panel({
-		id: 'meetupList',
-		layout: 'fit',
-		html: 'this is the meetup list container',
-		dockedItems: [rsvpApp.views.meetupToolbar]
-	});
-	
-	
-	rsvpApp.views.viewport = new Ext.Panel({
-		fullscreen: true,
-		layout: 'card',
-		cardAnimation: 'slide',
-		items: [rsvpApp.views.meetupListContainer]
-	});
+  name: 'rsvpApp',
+  useLoadMask: true,
+
+    onReady: function() {
+      var access_token = document.URL.match(/access_token=.+\b/g);
+      if (access_token) {
+        alert(access_token.toString().split('=')[1]);
+      } else {
+        // User not logged in, log them in
+        var client_id = '9vjs6vdesogbvnrv6uoal3ufh9';
+        if (document.URL.indexOf('localhost') !== -1) {
+          client_id = '5olqblvb85fcckedja4uv42cgt';
+        }
+        window.location = 'https://secure.meetup.com/oauth2/authorize?client_id=' + client_id + '&response_type=token&redirect_uri=' + encodeURIComponent(document.URL);
+      }
+    },
+
+  launch: function () {
+
+  rsvpApp.views.meetupToolbar = new Ext.Toolbar ({
+    id: 'meetupToolbar',
+    title: 'RSVP List'
+  });
+
+  rsvpApp.views.meetupListContainer = new Ext.Panel({
+    id: 'meetupList',
+    layout: 'fit',
+    html: 'this is the meetup list container',
+    dockedItems: [rsvpApp.views.meetupToolbar]
+  });
+
+  rsvpApp.views.viewport = new Ext.Panel({
+    fullscreen: true,
+    layout: 'card',
+    cardAnimation: 'slide',
+    items: [rsvpApp.views.meetupListContainer]
+  });
 }});
