b1136daab0287c889c740097f7ecc743cf797fc7
Luca Candela <lucacandela@Luca-Candelas-MacBook-Pro.local>
2011-07-02 18:27:31 -0700

    small fix

--- public/javascripts/rsvp.js
+++ public/javascripts/rsvp.js
@@ -1,10 +1,9 @@
-var RSVP = new Ext.Application({
+var App = new Ext.Application({
 	name: 'rsvp',
-	useLoadMask : true,
-	launch : function () {
+	useLoadMask: true,
+	launch: function () {
 	rsvp.views.viewport = new Ext.Panel({
-	fullscreen: true,
-	html:'this is a freakin viewport'
-	
+		fullscreen: true,
+		html:'this is a viewport'
 	});
-}
+}});
