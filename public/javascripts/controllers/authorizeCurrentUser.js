4ab86c2f5541b1c418e8217881f3317a034ed868
Luca Candela <luca.candela@gmail.com>
2011-07-05 14:15:59 -0700

    Added Auth code from old .js, file is not used yet.

---
+++ public/javascripts/controllers/authorizeCurrentUser.js
@@ -0,0 +1,13 @@
+onReady: function() {
+var access_token = document.URL.match(/access_token=.+\b/g);
+if (access_token) {
+alert(access_token.toString().split('=')[1]);
+} else {
+// User not logged in, log them in
+var client_id = '9vjs6vdesogbvnrv6uoal3ufh9';
+if (document.URL.indexOf('localhost') !== -1) {
+client_id = '5olqblvb85fcckedja4uv42cgt';
+}
+window.location = 'https://secure.meetup.com/oauth2/authorize?client_id=' + client_id + '&response_type=token&redirect_uri=' + encodeURIComponent(document.URL);
+}
+}
