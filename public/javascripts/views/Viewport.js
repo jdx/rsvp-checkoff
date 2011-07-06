2f4ee4ff55d5d0067395734defbd9be036c132c0
Luca Candela <luca.candela@gmail.com>
2011-07-05 22:15:55 -0700

    Now Viewport.js checks to see if the app is already authorized, brings the user to Meetup.com Auth page if negative, drops you in rsvpList if positive (this will have to change).

--- public/javascripts/views/Viewport.js
+++ public/javascripts/views/Viewport.js
@@ -4,14 +4,34 @@ rsvpBuddy.views.Viewport = Ext.extend(Ext.Panel, {
     layout: 'fit',
 
     initComponent: function() {
-
+	
+		var access_token = document.URL.match(/access_token=.+\b/g);
+		if (access_token) {
+			alert(access_token.toString().split('=')[1]);
+        // User already logged in, for now goes to authorizeUser view, should go to loggedUserDetails view
         Ext.apply(this, {
             items: [
-                rsvpBuddy.views.authorizeUser
+                rsvpBuddy.views.rsvpList
             ]
         });
 
         rsvpBuddy.views.Viewport.superclass.initComponent.apply(this, arguments);
-    }
+    } else {
+			// User not logged in, go to authorizeUser view
+			var client_id = '9vjs6vdesogbvnrv6uoal3ufh9';
+			if (document.URL.indexOf('localhost') !== -1) {
+				client_id = '5olqblvb85fcckedja4uv42cgt';
+				}
+				window.location = 'https://secure.meetup.com/oauth2/authorize?client_id=' + client_id + '&response_type=token&redirect_uri=' + encodeURIComponent(document.URL);
+				Ext.apply(this, {
+		            items: [
+		                rsvpBuddy.views.authorizeUser
+		            ]
+		        });
+
+		        rsvpBuddy.views.Viewport.superclass.initComponent.apply(this, arguments);
+				
+				}
+}
 
 });
